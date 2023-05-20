8d8082731051f4967c1d8ffdfecaf9bb483bff23
flavio1508 <flavioaugusto1508@gmail.com>
2023-05-20 20:09:06 -0300

    Criando logica para receber dependente menores que 18 anos de idade e criando teste para vericar essas condições

--- src/main/java/br/desafio/digix/models/Dependentes.java
+++ src/main/java/br/desafio/digix/models/Dependentes.java
@@ -1,10 +1,14 @@
 package br.desafio.digix.models;
+
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,7 +23,33 @@ public class Dependentes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 200)
     private String nome;
+    @Column(nullable = false)
+    private Genero genero;
+    @Column(nullable = false)
     private LocalDate dataDeNascimentoDependente;
+
+    @ManyToOne
+    private ResponsavelFamilia responsavelFamilia;
+
+    public Dependentes(String nome, Genero genero, LocalDate dataDeNascimentoDependente,
+            ResponsavelFamilia responsavelFamilia) {
+        this.nome = nome;
+        this.genero = genero;
+        this.dataDeNascimentoDependente = dataDeNascimentoDependente;
+        this.responsavelFamilia = responsavelFamilia;
+    }
+
+    @PrePersist
+    void verificarIdade() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate dataDezoitoAnosAtras = dataAtual.minusYears(18);
+
+        if (dataDeNascimentoDependente.isAfter(dataDezoitoAnosAtras)) {
+            throw new IllegalArgumentException("O dependente deve ser menor de 18 anos.");
+        }
+    }
 
 }
---
+++ src/test/java/br/desafio/digix/models/DependenteTest.java
@@ -0,0 +1,49 @@
+package br.desafio.digix.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class DependenteTest {
+
+    @Test
+    public void test_validacao_idade_dependente_menor_de_dezoito_anos() {
+        // Cria um responsável com a data de nascimento mais recente possível (hoje - 18
+        // anos)
+        LocalDate dataDeNascimentoResponsavel = LocalDate.now().minusYears(18);
+        ResponsavelFamilia responsavel = new ResponsavelFamilia("Nome Responsável", "email@example.com", "123456789",
+                dataDeNascimentoResponsavel, Genero.MASCULINO);
+
+        // Cria um dependente com a data de nascimento posterior à data limite (maior de
+        // 18 anos)
+        LocalDate dataDeNascimentoDependente = LocalDate.now().minusYears(18).plusDays(1);
+        Dependentes dependente = new Dependentes("Nome Dependente", Genero.FEMININO, dataDeNascimentoDependente,
+                responsavel);
+
+        // Tenta salvar o dependente, deve lançar uma exceção
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            dependente.verificarIdade();
+        });
+    }
+
+    @Test
+    public void test_validacao_idade_dependente_maior_de_dezoito_anos() {
+        // Cria um responsável com a data de nascimento mais recente possível (hoje - 18
+        // anos)
+        LocalDate dataDeNascimentoResponsavel = LocalDate.now().minusYears(18);
+        ResponsavelFamilia responsavel = new ResponsavelFamilia("Nome Responsável", "email@example.com", "123456789",
+                dataDeNascimentoResponsavel, Genero.MASCULINO);
+
+        // Cria um dependente com a data de nascimento anterior à data limite (menor de
+        // 18 anos)
+        LocalDate dataDeNascimentoDependente = LocalDate.now().minusYears(18).minusDays(1);
+        Dependentes dependente = new Dependentes("Nome Dependente", Genero.FEMININO, dataDeNascimentoDependente,
+                responsavel);
+
+        // Tenta salvar o dependente, não deve lançar exceção
+        Assertions.assertDoesNotThrow(() -> {
+            dependente.verificarIdade();
+        });
+    }
+}
